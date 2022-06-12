d9b8ab2a25c7cf34cf7a4b6cff433fc52aaf424e
Adam Kowalczyk <107359683+4kow@users.noreply.github.com>
2022-06-12 18:25:28 +0200

    File upload

---
+++ Plan zajęć/Plan zajęć/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Plan_zajęć
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(67, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Poniedziałek";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(284, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Wtorek";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(501, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sroda";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(719, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Czwartek";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(939, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(151, 31);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Piatek";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(67, 138);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(467, 450);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(623, 138);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(467, 450);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1153, 648);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Plan zajęć/Plan zajęć/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plan_zajęć
+{
+    public partial class Form1 : Form
+    {
+        public System.Windows.Forms.HorizontalAlignment TextAlign { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Text = "Adam:" + Environment.NewLine + Environment.NewLine + "Stacjonarne Zajęcia na poniedziałek (08:15 - 11:30):" + Environment.NewLine + Environment.NewLine + "08:15 - 11:30 Systemy wbudowane (L) dr. Rabczuk" + Environment.NewLine + Environment.NewLine + "Zajęcia odbędą się 10.05, 24.05, 07.06, 14.06";
+
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Text = "Julia:" + Environment.NewLine + Environment.NewLine + "Zajęcia na poniedziałek (10:15 - 14:45):"
+                + Environment.NewLine + Environment.NewLine + "10:15 - 12:00 Marketing towarów i usług (W)" + Environment.NewLine + Environment.NewLine + "Okienko" + Environment.NewLine + Environment.NewLine +
+                "13:00 - 14:45 Podstawy procesów technologicznych (W)";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Text = "Adam:" + Environment.NewLine + Environment.NewLine + "Zajęcia na wtorek (07:20 - 18:45) co2td (09:15 - 18:45)" + Environment.NewLine + Environment.NewLine + "07:20 - 08:50 Metody optymalizacji (L) dr.Miller - Co drugi tydzień"
+                + Environment.NewLine + Environment.NewLine + "09:00 - 10:30 Metody i narzędzia sztucznej inteligencji (L) dr.Skakovski" + Environment.NewLine + Environment.NewLine + 
+                "11:00 - 12:30 Grafika komputerowa i techniki multimedialne (L) mgr.Milewski" + Environment.NewLine + Environment.NewLine + "13:15 - 14:45 Wprowadzenie do baz danych (L) dr.Łuksza" 
+                + Environment.NewLine + Environment.NewLine + "15:15 - 16:45 Sieci komputerowe (L) mgr.Mieczyńska" + Environment.NewLine + Environment.NewLine + "17:15 - 18:45 Grafika komputerowa i techniki multimedialne (W) mgr.Milewski";
+
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Text = "Julia:" + Environment.NewLine + Environment.NewLine + "Stacjonarne zajęcia na wtorek (15:15 - 16:45)" + Environment.NewLine + Environment.NewLine +
+                "15:15 - 16:45 Analiza instrumentalna (L)";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Text = "Adam:" + Environment.NewLine + Environment.NewLine + "Zajęcia na środę (11:15 - 16:35):" + Environment.NewLine + Environment.NewLine + "11:15 - 12:45 Wprowadzenie do baz danych (W) mgr.Rosicka"
+                + Environment.NewLine + Environment.NewLine + "13:15 - 14:45 Teoria informacji (W) prof.Borys" + Environment.NewLine + Environment.NewLine + "15:00 - 16:35 j.Angielski mgr.Korta";
+
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Text = "Julia:" + Environment.NewLine + Environment.NewLine + "Zajęcia na środę (08:00 - 11:45)" + Environment.NewLine + Environment.NewLine + "08:00 - 09:30 Zarządzanie produktem (W)" + Environment.NewLine + Environment.NewLine +
+                "10:15 - 11:45 Zarządzanie produktem (ĆW)";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Text = "Adam:" + Environment.NewLine + Environment.NewLine + "Zajęcia na czwartek (09:15 - 11:45):" + Environment.NewLine + Environment.NewLine + "09:15 - 10:45 Teoria informacji (Ć) prof.Borys" + Environment.NewLine + Environment.NewLine +
+                "11:15 - 12:45 Sieci komputerowe (W) dr.Januszewski";
+
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Text = "Julia:" + Environment.NewLine + Environment.NewLine + "Zajęcia na czwartek (09:15 - 17:30):" + Environment.NewLine + Environment.NewLine +
+                "09:15 - 11:00 j.Angielski" + Environment.NewLine + Environment.NewLine + "11:15 - 12:45 Analiza instrumentalna (W)" + Environment.NewLine + Environment.NewLine + "13:15 - 15:00 Zdrowie publiczne (W)" + Environment.NewLine + Environment.NewLine + "15:15 - 17:30 Środowisko a zdrowie człowieka (W)";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Text = "Adam:" + Environment.NewLine + Environment.NewLine + "Stacjonarne Zajęcia na piątek (08:15 - 11:30):" + Environment.NewLine + Environment.NewLine + "08:15 - 11:30 Systemy wbudowane dr. Rabczuk" + Environment.NewLine + Environment.NewLine + "Zajęcia odbędą się 30.04, 14.05";
+
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Text = "Julia:" + Environment.NewLine + Environment.NewLine + "Zajęcia na piątek (09:15 - 10:45)" + Environment.NewLine + Environment.NewLine + "09:15 - 10:45 Marketing towarów i usług (ĆW)";
+        }
+    }
+}
